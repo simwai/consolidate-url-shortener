063799e0a9e7178da3a60619b1c73e359f8bb7ca
Simon Waiblinger <16225108+simwai@users.noreply.github.com>
2020-10-17 15:21:11 +0200

    changed folder structure

---
+++ server/app.ts
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express.Router()
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrlCode = req.params.shortUrl
+    const url = findUrlForShortcode()
+
+    try {
+        if (url) {
+            return res.redirect(url.longUrl)
+        } else {
+            return res.status(400).json('The short url doesn\'t exists in our system.')
+        }
+    }
+    catch (err) {
+        console.error('Error while retrieving long url for shorturlcode ' + shortUrlCode)
+        return res.status(500).json('There is some internal error.')
+    }
+})
+
+module.exports = app
